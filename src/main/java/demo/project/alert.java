1f6e5323578297faaf02ccd4c8c89d4eeafe5c26
lakshmi-sravanthi <96106566+lakshmi-sravanthi@users.noreply.github.com>
2021-12-21 13:02:57 +0530

    Update alert.java

--- src/main/java/demo/project/alert.java
+++ src/main/java/demo/project/alert.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class alert {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\satee\\Downloads\\chromedriver.exe");
 	     WebDriver driver=new ChromeDriver();
